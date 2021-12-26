816e55a3de683e32f6b69fb15a30c336a2b1e986
Adam Matteson <matteson211@hotmail.com>
2021-12-26 16:45:26 +0800

    updated two files

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,21 @@
 class UsersController < ApplicationController
+
+  def show
+  end
+
+  def find_user
+  end
+
+  def login
+  end
+
+  def update
+  end
+
+  private
+
+  # This is how we get the information from wechat platform
+  def wechat_user
+  end
+
 end
