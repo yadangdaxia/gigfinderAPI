b4efb0abc790a1c226370a064f08420dc5e6f358
Adam Matteson <matteson211@hotmail.com>
2021-12-28 22:34:35 +0800

    authentication now working

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
+      resources :users, only: [:index, :show, :update]
       # For login action, verb needs to be post
       post '/login', to: 'users#login'
     end
