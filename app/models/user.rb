44ebcbcc58ef7fa45f132303ffa8624aee88a9f0
Adam Matteson <matteson211@hotmail.com>
2022-01-18 21:37:41 +0800

    functioning with aliyun pics

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,10 @@
 class User < ApplicationRecord
-  has_many :gig_bookmarks
-  has_many :talent_bookmarks
+  has_many :gig_bookmarks, dependent: :destroy
+  has_many :talent_bookmarks, dependent: :destroy
   # No need to validate name, as we don't have it on login
   validate :open_id
   has_many :gigs
-  has_many :inquiries
+  has_many :inquiries, dependent: :destroy
   has_one_attached :image_attachment
 
   include PgSearch::Model
